075a1f3e8da44c4a9f71e341ffb30cfd16488f66
Navid <navidjalili3@gmail.com>
2023-08-01 23:34:47 +0330

    Implemented multi-threading with shared data modification

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ shared_data = []
 def modify_shared_data():
     with lock:
         shared_data.append("New data")
+
+
+def main():
+    threads = []
+    for _ in range(10):
+        thread = threading.Thread(target=modify_shared_data)
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print(shared_data)
+
+
+if __name__ == "__main__":
+    main()
